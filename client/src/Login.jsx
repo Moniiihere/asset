3488220160e8ebaa5cb44bdc37905bde32950a2a
Monisha <168123348+Moniiihere@users.noreply.github.com>
2024-11-19 12:59:16 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
     e.preventDefault();
 
     axios
-      .post("http://localhost:3001/login", { email, password })
+      .post("https://post-api2.onrender.com/login", { email, password })
       .then((result) => {
         console.log(result);
         if (result.data === "success") {
