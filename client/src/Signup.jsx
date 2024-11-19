29c29e7019161369448c8cd1ae23cd378eb30cd8
Monisha <168123348+Moniiihere@users.noreply.github.com>
2024-11-19 13:00:26 +0530

    Update Signup.jsx

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -11,7 +11,7 @@ function Signup() {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:3001/register", { name, email, password })
+      .post("https://post-api2.onrender.com/register", { name, email, password })
       .then((result) => {
         console.log(result);
         navigate("/login");
